5e66ba4e19588dd7b740b43b59a2ec86facfd9de
Basam Hassan <basammoses@gmail.com>
2023-05-02 15:46:16 -0400

    v2

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,8 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: '*' ,
+  origin: ["http://localhost:5174", "http://localhost:5173", "https://late-text-2-mu.vercel.app/"],
   credentials: true,
-
 
 
 }));
@@ -80,7 +79,7 @@ const server = app.listen(
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "*",
+    origin: ["http://localhost:5174", "http://localhost:5173", "https://late-text-2-mu.vercel.app/"],
     credentials: true,
   },
 });
